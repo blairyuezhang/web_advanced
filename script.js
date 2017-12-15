40c7b3499c23f099f8dfe23b12b6705f2a5faffb
blairyuezhang <blairz1995@gmail.com>
2017-12-15 14:01:37 -0500

    /

--- script.js
+++ script.js
@@ -2,10 +2,50 @@ $(document).ready(function(){
    $(".trigger").on("mouseover",function(){
       $(".bg").fadeIn();
   });
-  
+
    $(".trigger").on("mouseout",function(){
       $(".bg").fadeOut();
   });
-  
-  
+
+  $("#pjo").on("mouseover", function(){
+    $("#one").fadeIn();
+  })
+
+  $("#pjo").on("mouseout", function(){
+    $("#one").fadeOut();
+  })
+
+  $("#pj2").on("mouseover", function(){
+    $("#two").fadeIn();
+  })
+
+  $("#pj2").on("mouseout", function(){
+    $("#two").fadeOut();
+  })
+
+  $("#pj3").on("mouseover", function(){
+    $("#three").fadeIn();
+  })
+
+  $("#pj3").on("mouseout", function(){
+    $("#three").fadeOut();
+  })
+
+  $("#pj4").on("mouseover", function(){
+    $("#four").fadeIn();
+  })
+
+  $("#pj4").on("mouseout", function(){
+    $("#four").fadeOut();
+  })
+
+  $("#stp").on("mouseover", function(){
+    $("#thesis").fadeIn();
+  })
+
+  $("#stp").on("mouseout", function(){
+    $("#thesis").fadeOut();
+  })
+
+
 });
